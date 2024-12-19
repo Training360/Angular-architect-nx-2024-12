79c434f488653b7f2af06cd15c14446b019d31a3
cherryApp <j.cserko@gmail.com>
2024-12-19 12:29:52 +0100

    feat: module federation base

---
+++ nx-mod-fed/apps/product/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  imports: [CommonModule],
+  selector: 'app-product-entry',
+  template: `<h1>
+    Welcome to product!
+  </h1>`,
+})
+export class RemoteEntryComponent {}
